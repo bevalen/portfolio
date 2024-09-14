67e5648f61651ee1c7a8b1a2b01f121dac6716e5
Ben_Valentin <bjoel.valentin@gmail.com>
2024-09-14 13:22:57 -0400

    Updated Wedly image

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -209,9 +209,9 @@ export const DATA = {
           icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: "",
+      image: "/wedly.jpg",
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+        "",
     },
     {
       title: "Quivr",
